8aaab836e15086b34bb8ccebda145de2134b8f96
bridgelab <poojajadhav14331@gmail.com>
2016-02-25 18:55:19 +0530

    Insertionsort commited

--- InsertionSort.java
+++ InsertionSort.java
@@ -3,7 +3,30 @@ class InsertionSort
 {
 	public static void main(String args[])
 	{
-		
+		int[] arr1={10,20,5,3,5,90};
+		int[] arr2=doInsertionSort(arr1);
+	
+		for(int i:arr2)
+		{
+			System.out.print(i);
+			System.out.print(",");
+		}
+	}
+	public static int[] doInsertionSort(int[] input)
+	{
+		int temp,i,j;
+		for(i=1;i<input.length;i++)
+		{
+			for(j=i;j>0;j--)
+			{
+				if(input[j] < input[j-1])
+				{
+					temp=input[j];
+					input[j]=input[j-1];
+					input[j-1]=temp;			
+				}
+			}
+		}
+		return input;
 	}
 }
-
