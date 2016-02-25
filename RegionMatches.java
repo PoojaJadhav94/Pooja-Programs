89a485420d926827e9893be985da098a715878d3
bridgelab <poojajadhav14331@gmail.com>
2016-02-25 08:48:50 +0530

    Second commit

---
+++ RegionMatches.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class RegionMatches
+{
+	public static void main(String[] args) 
+	{
+        	String base;;
+        	String remove;
+        
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the string");
+		base=sc.next();
+		System.out.println("Enter the string you want to search:");
+		remove=sc.next();
+
+		int baseLength=base.length();
+        	int removeLength=remove.length();
+        	boolean foundIt=false;
+        
+		for (int i=0;i<=(baseLength-removeLength);i++) 
+		{
+           		if(base.regionMatches(i,remove,0,removeLength)) 
+			{
+              			foundIt = true;
+              			System.out.println(base.substring(i, i+removeLength));
+              			break;
+           		}
+        	}
+        	if(!foundIt)
+            		System.out.println("No match found.");
+    	}
+}
